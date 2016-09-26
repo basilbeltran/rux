6ee927817acbfec1c7da99f13156dc2622cdf7a7
Basil <Basil.beltran@gmail.com>
2016-09-26 12:59:04 -0600

    loops through questions

--- week2/victims-and-volunteers/main.js
+++ week2/victims-and-volunteers/main.js
@@ -6,28 +6,31 @@ experts = [];
 var i = 0;
 do{                                             // DO WHILE
   i++;
-  var vName = prompt(`Name ? ${i}`);
-  var vPhone = prompt(`What is your question ? ${i}`);
-  var vAddress = prompt(`Is this Urgent? ${i}`);
+  var qName = prompt(`Name ? ${i}`, "Basil");
+  var qQuestion = prompt(`What is your question ? ${i}`, "How many turtles are there?");
+  var qCategory = prompt(`Category of your question ${i}`, "cosmology");
+  var qUrgency = prompt(`Degree of urgency? ${i}`, "urgent");
   questions.push({"name":qName, "category":qCategory, "question":qQuestion, "urgency":qUrgency  });
 } while( confirm("add another question ?") );
 
 
 var i = 0;
 while( confirm("ADD AN EXPERT ?") ){     //  WHILE
-  var vName = prompt(`Name of expert  ${i}`);
-  var vPhone = prompt(`What is your area of expertice ${i}`);
-  var vAddress = prompt(`Current availability ${i}`);
+  var eName = prompt(`Name of expert  ${i}`, "Stephan");
+  var eArea = prompt(`What is your area of expertice ${i}`, "cosmology");
+  var eAvailable = prompt(`Current availability ${i}`, "available");
   experts.push({"name":eName, "area":eArea, "available":eAvailable  });
 }
 
 if (questions != null) {
-  for(question in questions){
-    document.getElementById("divQuestions").innerHTML =   question.qName
+  //for(question in questions){
+  for (var i = 0; i < questions.length; i++) {
+    document.getElementById("divQuestions").innerHTML =   questions[i].name
                                                         + " is asking an "
-                                                        + question.qUrgency
-                                                        + " question :"
-                                                        + question.qQuestion;
+                                                        + questions[i].urgency
+                                                        + " question related to "
+                                                        + questions[i].category + " : "
+                                                        + questions[i].question;
   }
 }
 
