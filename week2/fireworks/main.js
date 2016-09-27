6b1f289736b43405cab60b822ff57ad9897d9cd1
Basil <Basil.beltran@gmail.com>
2016-09-27 12:44:55 -0600

    added mad metrics to portfolio

---
+++ week2/fireworks/main.js
@@ -0,0 +1,15 @@
+
+function launchRocket(){
+  var rocket = document.createElement('div')
+  rocket.style.left   = "50vw";
+  rocket.style.bottom = "0vh";
+  var then = performance.now()
+
+
+}
+
+calcPosition(){
+  var now = performance.now();
+  var timeElapsed = then - now;
+  console.log(timeElapsed);
+}
